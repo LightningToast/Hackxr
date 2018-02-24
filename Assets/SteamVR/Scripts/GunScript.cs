6e6b66d9759d0646632fa5d63d1265952fb3d2d1
Justin Park <justindypark@yahoo.com>
2018-02-24 17:26:58 -0600

    Initial commit

---
+++ Assets/SteamVR/Scripts/GunScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZEffects;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject controllerRight;
+
+    private SteamVR_TrackedObject trackObj;
+    private SteamVR_Controller.Device device;
+
+    private SteamVR_TrackedController controller;
+
+    public EffectTracer TracerEffect;
+    public Transform muzzleTransform;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = controllerRight.GetComponent<SteamVR_TrackedController>();
+        
+        trackObj = controllerRight.GetComponent<SteamVR_TrackedObject>();
+    }
+    private void TriggerPressed(object sender, ClickedEventArgs o)
+    {
+        Debug.Log("shoot");
+        ShootWeapon();
+    }
+    private void ShootWeapon()
+    {
+        RaycastHit hit = new RaycastHit();
+        Ray ray = new Ray(muzzleTransform.position, muzzleTransform.forward);
+
+        device = SteamVR_Controller.Input((int)trackObj.index);
+        device.TriggerHapticPulse(750);
+        TracerEffect.ShowTracerEffect(muzzleTransform.position, muzzleTransform.forward, 250f);
+
+        if (Physics.Raycast(ray, out hit, 5000f))
+        {
+            if (hit.collider.attachedRigidbody)
+            {
+
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

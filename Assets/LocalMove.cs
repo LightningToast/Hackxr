a301518f89c9784495542e752eb3234dd8b60cc6
Justin Park <justindypark@yahoo.com>
2018-02-25 11:42:37 -0600

    dasfads

---
+++ Assets/LocalMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalMove : MonoBehaviour {
+    GameObject childRig;
+	// Use this for initialization
+	void Start () {
+        childRig = transform.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = childRig.transform.position;
+	}
+}

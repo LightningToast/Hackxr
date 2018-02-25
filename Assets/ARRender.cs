82fc9ed684ee610fa50325eb4ace710891528020
Justin Park <jep067@ucsd.edu>
2018-02-25 01:28:01 -0800

    added rendering options

--- Assets/ARRender.cs
+++ Assets/ARRender.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ARRender : MonoBehaviour {
-
+    bool active = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,17 @@ public class ARRender : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(active) {
+            this.GetComponent<Renderer>().enabled = true;
+        } else {
+            this.GetComponent<Renderer>().enabled = false;
+        }
+        active = false;
 	}
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("Vision")) {
+            active = true;
+        }
+    }
 }

288591957314d1018c0396678fb1485d092fb438
Justin Park <justindypark@yahoo.com>
2018-02-25 02:49:00 -0600

    added guns

---
+++ Assets/BulletTrait.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTrait : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("SelfDestruct",2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject controllerRight;
+    public Transform crosshair;
+    public GameObject projectile;
+    private SteamVR_TrackedController controller;
+    public float bulletSpeed;
+    void Start()
+    {
+        controller = controllerRight.GetComponent<SteamVR_TrackedController>();
+        controller.TriggerClicked += Shoot;
+    }
+    private void Shoot(object sender, ClickedEventArgs e)
+    {
+        if (projectile)
+        {
+            GameObject newProjectile = Instantiate(projectile, crosshair.gameObject.transform.position, transform.rotation) as GameObject;
+            newProjectile.GetComponent<Rigidbody>().AddForce(crosshair.gameObject.transform.forward * bulletSpeed, ForceMode.VelocityChange);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

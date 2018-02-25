ffcf4cc1e3e774a658d22f5c73198253bd14343a
Justin Park <jep067@ucsd.edu>
2018-02-25 01:19:48 -0800

    add ring move to separate class

--- Assets/RingExtend.cs
+++ Assets/RingExtend.cs
@@ -1,33 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class RingExtend : NetworkBehaviour {
+public class RingExtend : MonoBehaviour {
     public float maxExtend;
     public float extendSpeed = 0.01f;
     public float extendVal = 0;
-    GameObject VRRig;
 	// Use this for initialization
 	void Start () {
-        if(!hasAuthority) {
-            return;
-        }
-        VRRig = GameObject.Find("LocalPlayer");
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
         transform.localScale = new Vector3(0.1f, 0.1f, 0) * extendVal + new Vector3(0, 0, 0.1f);
 
         extendVal += extendSpeed;
         if(extendVal > maxExtend) {
             extendVal = 0.1f;
         }
-        if(!hasAuthority) {
-            return;
-        }
-        transform.position = VRRig.transform.GetChild(0).position;
 	}
 }
---
+++ Assets/RingMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RingMove : NetworkBehaviour
+{
+    public float maxExtend;
+    public float extendSpeed = 0.01f;
+    public float extendVal = 0;
+    GameObject VRRig;
+    // Use this for initialization
+    void Start()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+        VRRig = GameObject.Find("LocalPlayer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+        transform.position = VRRig.transform.GetChild(0).position;
+    }
+}

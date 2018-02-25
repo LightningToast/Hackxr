d3dbfb42880f18973d42ea3c706462b13b18cd5f
Justin Park <justindypark@yahoo.com>
2018-02-25 06:14:03 -0600

    added gun  visuals

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,22 +7,38 @@ public class Bullet : MonoBehaviour
     public GameObject controllerRight;
     public Transform crosshair;
     public GameObject projectile;
+    public GameObject rechargeAnim;
+    public float timeDelay = 1.0f;
+    bool coolDown = false;
     private SteamVR_TrackedController controller;
+
     public float bulletSpeed;
     void Start()
     {
+        rechargeAnim.SetActive(false);
         controller = controllerRight.GetComponent<SteamVR_TrackedController>();
         controller.TriggerClicked += Shoot;
     }
     private void Shoot(object sender, ClickedEventArgs e)
     {
-        if (projectile)
+        if (!coolDown)
         {
-            GameObject newProjectile = Instantiate(projectile, crosshair.gameObject.transform.position, transform.rotation) as GameObject;
-            newProjectile.GetComponent<Rigidbody>().AddForce(crosshair.gameObject.transform.forward * bulletSpeed, ForceMode.VelocityChange);
+            if (projectile)
+            {
+                GameObject newProjectile = Instantiate(projectile, crosshair.gameObject.transform.position, transform.rotation) as GameObject;
+                newProjectile.GetComponent<Rigidbody>().AddForce(crosshair.gameObject.transform.forward * bulletSpeed, ForceMode.VelocityChange);
+            }
         }
+        StartCoroutine(cool());
     }
-
+    IEnumerator cool()
+    {
+        coolDown = true;
+        rechargeAnim.SetActive(true);
+        yield return new WaitForSeconds(timeDelay);
+        rechargeAnim.SetActive(false);
+        coolDown = false;
+    }
     // Update is called once per frame
     void Update()
     {
